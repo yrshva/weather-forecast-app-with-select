676c284a34926c105b3be7aed23e64e42077a1c4
jesjonesc <58461507+jesjonesc@users.noreply.github.com>
2022-07-15 19:40:15 +0300

    updated github link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="footer-github-link">
-      <a href="https://github.com/jesjonesc/weather-app">See on GitHub</a>
+      <a href="https://github.com/jesjonesc/weather-forecast-app-with-select">See on GitHub</a>
     </div>
   );
 }
